992f84ee693081629c27a805825542c4ca53eba6
SakuraOnozuka <onozuka.sakura.23e@st.kyoto-u.ac.jp>
2021-07-02 19:57:27 +0900

    ファイル作成

---
+++ front/src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about">
+    <button @click="click">click</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component({
+})
+export default class About extends Vue {
+  click():void{
+    this.$router.push('/')
+  }
+}
+</script>
---
+++ front/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img src="http://localhost:9001/video_feed">
+    <button @click="click">click</button>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import axios from 'axios'
+const CancelToken = axios.CancelToken;
+@Component({
+})
+export default class Home extends Vue {
+  click():void{
+    axios.get(`http://localhost:9001/video_feed`, {
+      cancelToken: CancelToken.source().token
+    })
+    CancelToken.source().cancel('Operation canceled by the user.');
+    this.$router.push('/about')
+  }
+}
+</script>
